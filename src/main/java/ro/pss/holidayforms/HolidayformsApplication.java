ce3704a0851ef32f7973ecddec0e520e48be36af
lucianpalaghe <l.palaghe@gmail.com>
2019-07-26 10:11:55 +0300

    Initial commit

---
+++ src/main/java/ro/pss/holidayforms/HolidayformsApplication.java
@@ -0,0 +1,13 @@
+package ro.pss.holidayforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayformsApplication.class, args);
+	}
+
+}
