718b49ad457982c027c667cbd6bb88065b2d5e12
lucianpalaghe <l.palaghe@gmail.com>
2019-08-13 15:19:53 +0300

    Removed test code

--- src/main/java/ro/pss/holidayforms/integrations/jira/JiraUserDetailsService.java
+++ src/main/java/ro/pss/holidayforms/integrations/jira/JiraUserDetailsService.java
@@ -6,14 +6,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import ro.pss.holidayforms.domain.User;
 import ro.pss.holidayforms.domain.repo.UserRepository;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -35,34 +35,37 @@ public class JiraUserDetailsService {
 
 	//	@PostConstruct
 	public void loadAllJiraUsers() throws IOException {
-//		log.info(String.format("Preparing to load all JIRA users from: %s", jiraUsersApiUrl));
-//		RestTemplate restTemplate = new RestTemplate();
-//		HttpHeaders httpHeaders = new HttpHeaders();
-//		httpHeaders.setBasicAuth(apiUsername, apiKey);
-//
-//		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(jiraUsersApiUrl)
-//				.queryParam("username", "")
-//				.queryParam("startAt", 0)
-//				.queryParam("maxResults", 1000);
-//		String getUrl = builder.toUriString();
-//
-//		HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
-//
-//		log.info(String.format("GETing JSON from: %s", getUrl));
-//		ResponseEntity<String> response = restTemplate.exchange(getUrl, HttpMethod.GET, httpEntity, String.class);
-//		if(response.getStatusCode() != HttpStatus.OK) {
-//			log.error(String.format("Response from JIRA is: %d %s", response.getStatusCode(), response.getBody()));
-//			return;
-//		}
-//		log.info(String.format("Response from %s: %s", jiraUsersApiUrl, response));
+		log.info(String.format("Preparing to load all JIRA users from: %s", jiraUsersApiUrl));
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setBasicAuth(apiUsername, apiKey);
 
-		File resource = new ClassPathResource("pss_users_jira.json").getFile();
-		String employees = new String(Files.readAllBytes(resource.toPath()));
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(jiraUsersApiUrl)
+				.queryParam("username", "")
+				.queryParam("startAt", 0)
+				.queryParam("maxResults", 1000);
+		String getUrl = builder.toUriString();
 
+		HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+
+		log.info(String.format("GETing JSON from: %s", getUrl));
+		ResponseEntity<String> response = restTemplate.exchange(getUrl, HttpMethod.GET, httpEntity, String.class);
+		if (response.getStatusCode() != HttpStatus.OK) {
+			log.error(String.format("Response from JIRA is: %d %s", response.getStatusCode(), response.getBody()));
+			return;
+		}
+		log.info(String.format("Response from %s: %s", jiraUsersApiUrl, response));
+
+//		File resource = new ClassPathResource("pss_users_jira.json").getFile();
+//		String jiraDetails = new String(Files.readAllBytes(resource.toPath()));
+		populateUsersWithJiraDetails(response.getBody());
+	}
+
+	private void populateUsersWithJiraDetails(String jsonDetails) throws IOException {
 		ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
-		List<JiraUserDetails> jiraUserList = objectMapper.readValue(employees, new TypeReference<List<JiraUserDetails>>() {
+		List<JiraUserDetails> jiraUserList = objectMapper.readValue(jsonDetails, new TypeReference<List<JiraUserDetails>>() {
 		});
 		jiraUserList = jiraUserList.stream()
 				.filter(u -> u.getAccountType().equals("atlassian"))
@@ -75,6 +78,5 @@ public class JiraUserDetailsService {
 				.collect(toList());
 
 		userRepo.saveAll(userList);
-//		objectMapper.writeValue(new File("output_employees.json"), jiraUserList);
 	}
 }
