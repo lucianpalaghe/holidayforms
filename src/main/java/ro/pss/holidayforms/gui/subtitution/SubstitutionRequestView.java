b2df969ec8212b9b5ee8f5b27984181c4bac740d
lucianpalaghe <l.palaghe@gmail.com>
2019-08-20 17:54:43 +0300

    Added holiday request comments in the approval and substitution views, tweaked responsive buttons, changed message file encoding

--- src/main/java/ro/pss/holidayforms/gui/subtitution/SubstitutionRequestView.java
+++ src/main/java/ro/pss/holidayforms/gui/subtitution/SubstitutionRequestView.java
@@ -7,10 +7,12 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Emphasis;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
@@ -37,11 +39,11 @@ import java.util.List;
 @Route(value = "substitutions", layout = HolidayAppLayout.class)
 @StyleSheet("responsive-buttons.css")
 public class SubstitutionRequestView extends HorizontalLayout implements AfterNavigationObserver, Broadcaster.BroadcastListener {
+	private final Grid<SubstitutionRequest> grid;
+	private final H2 heading;
 	@Autowired
 	private HolidaySubstitutionService service;
-	private final Grid<SubstitutionRequest> grid;
 	private HolidayConfirmationDialog holidayConfDialog;
-	private final H2 heading;
 
 	public SubstitutionRequestView() {
 		this.grid = new Grid<>();
@@ -50,7 +52,7 @@ public class SubstitutionRequestView extends HorizontalLayout implements AfterNa
 		grid.addColumn(r -> r.getRequest().getDateFrom()).setHeader(MessageRetriever.get("appViewGridHeaderStart")).setFlexGrow(1);
 		grid.addColumn(new ComponentRenderer<>(this::getActionButtons)).setFlexGrow(2);
 		grid.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT, GridVariant.LUMO_ROW_STRIPES);
-
+		grid.setItemDetailsRenderer(getRequestInfoRenderer());
 		heading = new H2();
 		heading.setVisible(false);
 
@@ -111,6 +113,28 @@ public class SubstitutionRequestView extends HorizontalLayout implements AfterNa
 			btnDeny.setText(MessageRetriever.get("deniedTxt"));
 			return new HorizontalLayout(btnDeny);
 		}
+	}
+
+	private ComponentRenderer<HorizontalLayout, SubstitutionRequest> getRequestInfoRenderer() {
+		return new ComponentRenderer<>(substitutionRequest -> {
+			HorizontalLayout detailsContainer = new HorizontalLayout();
+			detailsContainer.setWidthFull();
+
+			String comments = substitutionRequest.getRequest().getComments();
+			if (comments.isEmpty()) {
+				comments = MessageRetriever.get("msgNoAdditionalComments");
+				Emphasis em = new Emphasis();
+				em.setText(comments);
+				detailsContainer.add(em);
+				return detailsContainer;
+			}
+			TextArea areaComments = new TextArea();
+			areaComments.setValue(comments);
+			areaComments.setReadOnly(true);
+			areaComments.setWidthFull();
+			detailsContainer.add(areaComments);
+			return detailsContainer;
+		});
 	}
 
 	private void rejectHolidaySubstitution(SubstitutionRequest request) {
