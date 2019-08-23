72565ecab9b85feaf58b55d87881e8524962ec45
aen <alexandru.enescu@pss.ro>
2019-08-23 17:15:15 +0300

    Improved the style for the export excel file

--- src/main/java/ro/pss/holidayforms/excel/ExcelExporter.java
+++ src/main/java/ro/pss/holidayforms/excel/ExcelExporter.java
@@ -32,6 +32,13 @@ public class ExcelExporter {
     @Autowired
     private UserRepository userRepository;
 
+    private static int COLUMN_NAME_FONT_HEIGHT = 14;
+    private static boolean COLUMN_NAME_FONT_BOLD = true;
+    private static int CONTENT_FONT_HEIGHT = 12;
+    private static boolean CONTENT_FONT_BOLD = false;
+    private static int COLUMN_NAME_WIDTH_CHAR = 30;
+    private static int COLUMNS_WIDTH_CHAR = 15;
+
     public byte[] doGetExcelByteArray() {
         Workbook workbook = new XSSFWorkbook();
         populateWorkbookWithData(workbook);
@@ -41,17 +48,19 @@ public class ExcelExporter {
     private void populateWorkbookWithData(Workbook workbook) {
         try {
             Sheet sheet = workbook.createSheet(MessageRetriever.get("sheetName"));
+            sheet.setDefaultColumnWidth(15); // characters
             int startRow = 1;
 
             // set the column names
             Row row = sheet.createRow(0);
+            row.setHeight((short) (4 * sheet.getDefaultRowHeight()));
             Cell cell = null;
             String[] sheetColNames = MessageRetriever.get("sheetColNames").split(",");
             int i;
             for (i = 0; i < sheetColNames.length; i++) {
                 cell = row.createCell(i);
                 cell.setCellValue(workbook.getCreationHelper().createRichTextString(sheetColNames[i]));
-                cell.setCellStyle(getCellStyleForColumnNames(workbook));
+                cell.setCellStyle(getCellStyleForColumnNamesGrey(workbook));
             }
 
             String[] monthsColNames = MessageRetriever.get("monthsNamesShort").split(",");
@@ -60,21 +69,21 @@ public class ExcelExporter {
                 //planning
                 cell = row.createCell(cnt++);
                 cell.setCellValue(workbook.getCreationHelper().createRichTextString(monthsColNames[j]) + " " + MessageRetriever.get("sheetPlanning"));
-                cell.setCellStyle(getCellStyleForColumnNames(workbook));
+                cell.setCellStyle(getCellStyleForColumnNamesMonths(cnt - 1, workbook));
                 //request
                 cell = row.createCell(cnt++);
                 cell.setCellValue(workbook.getCreationHelper().createRichTextString(monthsColNames[j]) + " " + MessageRetriever.get("sheetRequest"));
-                cell.setCellStyle(getCellStyleForColumnNames(workbook));
+                cell.setCellStyle(getCellStyleForColumnNamesMonths(cnt - 1, workbook));
             }
             cell = row.createCell(cnt++);
             cell.setCellValue(workbook.getCreationHelper().createRichTextString(MessageRetriever.get("sheetColTotalControl") + " " + MessageRetriever.get("sheetPlanning")));
-            cell.setCellStyle(getCellStyleForColumnNames(workbook));
+            cell.setCellStyle(getCellStyleForColumnNamesMonths(cnt - 1, workbook));
             cell = row.createCell(cnt++);
             cell.setCellValue(workbook.getCreationHelper().createRichTextString(MessageRetriever.get("sheetColTotalControl") + " " + MessageRetriever.get("sheetRequest")));
-            cell.setCellStyle(getCellStyleForColumnNames(workbook));
+            cell.setCellStyle(getCellStyleForColumnNamesMonths(cnt - 1, workbook));
             cell = row.createCell(cnt);
             cell.setCellValue(workbook.getCreationHelper().createRichTextString(MessageRetriever.get("sheetColNotUsedDays")));
-            cell.setCellStyle(getCellStyleForColumnNames(workbook));
+            cell.setCellStyle(getCellStyleForColumnNamesGrey(workbook));
 
             // populate with data
             List<User> users = userRepository.findAll();
@@ -84,40 +93,50 @@ public class ExcelExporter {
                 row = sheet.createRow(u + startRow);
                 cell = row.createCell(col++);
                 cell.setCellValue(workbook.getCreationHelper().createRichTextString((u + startRow) + ""));
+                cell.setCellStyle(getCellStyleForContentGrey(workbook));
                 cell = row.createCell(col++);
                 cell.setCellValue(workbook.getCreationHelper().createRichTextString(user.getName()));
+                cell.setCellStyle(getCellStyleForContentUserNamesAndCO(workbook));
                 cell = row.createCell(col++);
                 cell.setCellValue(workbook.getCreationHelper().createRichTextString(user.getAvailableVacationDays() + ""));
-
+                cell.setCellStyle(getCellStyleForContentUserNamesAndCO(workbook));
                 DashboardData data = dataService.getDashboardData(user.getEmail());
                 for (int m = 0; m < MessageRetriever.get("monthsNamesShort").split(",").length; m++) {
                     cell = row.createCell(col++);
                     cell.setCellValue(workbook.getCreationHelper().createRichTextString(data.getChartPlannedDays()[m] == 0 ? "" : data.getChartPlannedDays()[m] + ""));
+                    cell.setCellStyle(getCellStyleForContentMonths(col - 1, workbook));
                     cell = row.createCell(col++);
                     cell.setCellValue(workbook.getCreationHelper().createRichTextString(data.getChartVacationDays()[m] == 0 ? "" : data.getChartVacationDays()[m] + ""));
+                    cell.setCellStyle(getCellStyleForContentMonths(col - 1, workbook));
                 }
                 int plannedDays = IntStream.of(data.getChartPlannedDays()).sum();
                 int requestDays = IntStream.of(data.getChartVacationDays()).sum();
                 cell = row.createCell(col++);
                 cell.setCellValue(workbook.getCreationHelper().createRichTextString(plannedDays + ""));
+                cell.setCellStyle(getCellStyleForContentMonths(col - 1, workbook));
                 cell = row.createCell(col++);
                 cell.setCellValue(workbook.getCreationHelper().createRichTextString(requestDays + ""));
+                cell.setCellStyle(getCellStyleForContentMonths(col - 1, workbook));
                 cell = row.createCell(col);
                 cell.setCellValue(workbook.getCreationHelper().createRichTextString((user.getAvailableVacationDays() - requestDays) + ""));
+                cell.setCellStyle(getCellStyleForContentGrey(workbook));
             }
             autosizeAllColumns(sheet);
-         //   sheet.setAutoFilter(new org.apache.poi.ss.util.CellRangeAddress(0, 0, 0, 30));
-            sheet.createFreezePane(0, 1);
+            // sheet.setAutoFilter(new org.apache.poi.ss.util.CellRangeAddress(0, 0, 0, 30));
+            sheet.createFreezePane(3, 1, 3, 1);
             log.info("excel file exported succesfully");
         } catch (Exception e) {
             log.error("error making excel", e);
         }
-
     }
 
     private static void autosizeAllColumns(Sheet sheet) {
         for (int i = 0; i < sheet.getLastRowNum(); i++) {
-            sheet.autoSizeColumn(i);
+            if (i == 1) {
+                sheet.setColumnWidth(i, 256 * COLUMN_NAME_WIDTH_CHAR);
+            } else {
+                sheet.setColumnWidth(i, 256 * COLUMNS_WIDTH_CHAR);
+            }
         }
     }
 
@@ -137,14 +156,100 @@ public class ExcelExporter {
         return wbAttach.toByteArray();
     }
 
-    private static CellStyle getCellStyleForColumnNames(Workbook workbook) {
+    private static CellStyle getCellStyleForColumnNamesGrey(Workbook workbook) {
         XSSFWorkbook xssfWorkbook = (XSSFWorkbook) workbook;
         XSSFCellStyle style = xssfWorkbook.createCellStyle();
         style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+        style.setFillPattern(FillPatternType.FINE_DOTS);
+        style.setWrapText(true);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.CENTER);
+        style.setBorderRight(BorderStyle.MEDIUM);
+        style.setBorderBottom(BorderStyle.MEDIUM);
+        XSSFFont font = xssfWorkbook.createFont();
+        font.setFontHeight(COLUMN_NAME_FONT_HEIGHT);
+        font.setBold(COLUMN_NAME_FONT_BOLD);
+        style.setFont(font);
+        return style;
+    }
+
+    private static CellStyle getCellStyleForColumnNamesMonths(int colIndex, Workbook workbook) {
+        XSSFWorkbook xssfWorkbook = (XSSFWorkbook) workbook;
+        XSSFCellStyle style = xssfWorkbook.createCellStyle();
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.CENTER);
+        style.setWrapText(true);
+        if (colIndex % 2 == 1) {
+            style.setFillForegroundColor(IndexedColors.LEMON_CHIFFON.getIndex());
+            style.setBorderRight(BorderStyle.THIN);
+        } else {
+            style.setFillForegroundColor(IndexedColors.LIGHT_TURQUOISE.getIndex());
+            style.setBorderRight(BorderStyle.MEDIUM);
+        }
+        style.setBorderBottom(BorderStyle.MEDIUM);
         style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         XSSFFont font = xssfWorkbook.createFont();
-        font.setFontHeight(13);
-        font.setBold(true);
+        font.setFontHeight(COLUMN_NAME_FONT_HEIGHT);
+        font.setBold(COLUMN_NAME_FONT_BOLD);
+        style.setFont(font);
+        return style;
+    }
+
+    private static CellStyle getCellStyleForContentGrey(Workbook workbook) {
+        XSSFWorkbook xssfWorkbook = (XSSFWorkbook) workbook;
+        XSSFCellStyle style = xssfWorkbook.createCellStyle();
+        style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+        style.setFillPattern(FillPatternType.FINE_DOTS);
+        style.setWrapText(true);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.CENTER);
+        style.setBorderRight(BorderStyle.MEDIUM);
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderBottom(BorderStyle.THIN);
+        XSSFFont font = xssfWorkbook.createFont();
+        font.setFontHeight(CONTENT_FONT_HEIGHT);
+        font.setBold(CONTENT_FONT_BOLD);
+        style.setFont(font);
+        return style;
+    }
+
+    private static CellStyle getCellStyleForContentUserNamesAndCO(Workbook workbook) {
+        XSSFWorkbook xssfWorkbook = (XSSFWorkbook) workbook;
+        XSSFCellStyle style = xssfWorkbook.createCellStyle();
+        style.setFillForegroundColor(IndexedColors.LIGHT_ORANGE.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        style.setWrapText(true);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.CENTER);
+        style.setBorderRight(BorderStyle.MEDIUM);
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderBottom(BorderStyle.THIN);
+        XSSFFont font = xssfWorkbook.createFont();
+        font.setFontHeight(CONTENT_FONT_HEIGHT);
+        font.setBold(CONTENT_FONT_BOLD);
+        style.setFont(font);
+        return style;
+    }
+
+    private static CellStyle getCellStyleForContentMonths(int colIndex, Workbook workbook) {
+        XSSFWorkbook xssfWorkbook = (XSSFWorkbook) workbook;
+        XSSFCellStyle style = xssfWorkbook.createCellStyle();
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.CENTER);
+        style.setWrapText(true);
+        if (colIndex % 2 == 1) {
+            style.setFillForegroundColor(IndexedColors.LEMON_CHIFFON.getIndex());
+            style.setBorderRight(BorderStyle.THIN);
+        } else {
+            style.setFillForegroundColor(IndexedColors.LIGHT_TURQUOISE.getIndex());
+            style.setBorderRight(BorderStyle.MEDIUM);
+        }
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderBottom(BorderStyle.THIN);
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        XSSFFont font = xssfWorkbook.createFont();
+        font.setFontHeight(CONTENT_FONT_HEIGHT);
+        font.setBold(CONTENT_FONT_BOLD);
         style.setFont(font);
         return style;
     }
